dc0a76764e3959b309c0c4966cb17c8c98b72531
marcellabecker <marcellabecker1@gmail.com>
2021-12-05 20:46:18 -0300

    ajuste

--- 4- Recursive Palindrome.py
+++ 4- Recursive Palindrome.py
@@ -1,14 +1,5 @@
-from typing import Text
-
-#retorna o palindromo
-def palindrome(text):
-    return text == text[::-1]
-#testa se é ou um palindromo
-def main():
-    text = input("Input: ")
-    if(palindrome(test)): 
-     print(text + " é um palindromo")
-    else:
-     print(text + " não é palindromo")
-        
-if __name__ == "__main__": main()
+word = input("Input: ")
+if str(word) == "".join(reversed(word)) :
+    print("Palindromo")
+else:
+    print("Não é um Palindromo")
